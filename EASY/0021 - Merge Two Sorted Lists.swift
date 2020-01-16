14c8701848b77632bee2f79f4555fe659abed6c8
Rudrank <rudrankriyam@gmail.com>
2020-01-16 06:17:13 +0530

    0021 - Merge Two Sorted Lists

---
+++ EASY/0021 - Merge Two Sorted Lists.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+class Solution {
+    func mergeTwoLists(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
+        guard let l1 = l1 else {
+            return l2
+        }
+        guard let l2 = l2 else {
+            return l1
+        }
+        if l1.val <= l2.val {
+            l1.next = mergeTwoLists(l1.next, l2)
+            return l1
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next)
+            return l2
+        }
+    }
+}
