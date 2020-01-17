35af57311b4af6e4985083a6cd8826bff2c28e81
Rudrank <rudrankriyam@gmail.com>
2020-01-17 16:18:20 +0530

    0104 - Maximum Depth of Binary Tree

---
+++ EASY/0104 - Maximum Depth of Binary Tree.swift
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class Solution {
+    func maxDepth(_ root: TreeNode?) -> Int {
+        guard let root = root else { return 0 }
+        return max(maxDepth(root.left), maxDepth(root.right)) + 1
+     }
+}
