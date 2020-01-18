489061afbffe91a8dc693dbda6333d105422a9b0
Rudrank <rudrankriyam@gmail.com>
2020-01-18 09:06:32 +0530

    0036 - Valid Sudoku Alternate solutions

--- MEDIUM/0036 - Valid Sudoku.swift
+++ MEDIUM/0036 - Valid Sudoku.swift
@@ -1,19 +1,81 @@
+// Solution One
+
 class Solution {
     func isValidSudoku(_ board: [[Character]]) -> Bool {
         if board == nil || board.count == 0 { return false }
+
         var rowSets = Array(repeating: Set<Character>(), count: 9)
-        var colSets = rowSets
-        var boxSets = rowSets
+        var columnSets = Array(repeating: Set<Character>(), count: 9)
+        var boxSets = Array(repeating: Set<Character>(), count: 9)
         
-        for row in board.indices {
-            for col in board[0].indices {
-                let digit = board[row][col]
+        for rowIndex in board.indices {
+            for columnIndex in board[0].indices {
+                let digit = board[rowIndex][columnIndex]
+                let boxIndex = (rowIndex / 3) * 3 + columnIndex / 3
+
                 if digit == "." { continue }
-                let box = (row / 3) * 3 + col / 3
-                if rowSets[row].contains(digit) || colSets[col].contains(digit) || boxSets[box].contains(digit) { return false }
-                rowSets[row].insert(digit)
-                colSets[col].insert(digit)
-                boxSets[box].insert(digit)
+
+                if rowSets[rowIndex].contains(digit) || columnSets[columnIndex].contains(digit) || boxSets[boxIndex].contains(digit) {
+                    return false
+                }
+
+                rowSets[rowIndex].insert(digit)
+                columnSets[columnIndex].insert(digit)
+                boxSets[boxIndex].insert(digit)
+            }
+        }
+        return true
+    }
+}
+
+// Solution Two
+
+class Solution {
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var rowSets = Array(repeating: Set<Character>(), count: 9)
+        var columnSets = Array(repeating: Set<Character>(), count: 9)
+        var boxSets = Array(repeating: Set<Character>(), count: 9)
+
+        for rowIndex in board.indices {
+            for columnIndex in board[0].indices {
+                let digit = board[rowIndex][columnIndex]
+                let boxIndex = (rowIndex / 3) * 3 + columnIndex / 3
+
+                if digit == "." { continue }
+
+                if !rowSets[rowIndex].insert(digit).inserted { return false }
+
+                if !columnSets[columnIndex].insert(digit).inserted { return false }
+
+                if !boxSets[boxIndex].insert(digit).inserted { return false }
+            }
+        }
+        return true
+    }
+}
+
+// Solution Three
+
+class Solution {
+    func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var rows = Array(repeating: [Character: Int](), count: 9)
+        var columns = Array(repeating: [Character: Int](), count: 9)
+        var boxes = Array(repeating: [Character: Int](), count: 9)
+
+        for rowIndex in board.indices {
+            for columnIndex in board[0].indices {
+                let digit = board[rowIndex][columnIndex]
+                let boxIndex = (rowIndex / 3) * 3 + columnIndex / 3
+
+                if digit == "." { continue }
+
+                rows[rowIndex][digit, default: 0] += 1
+                columns[columnIndex][digit, default: 0] += 1
+                boxes[boxIndex][digit, default: 0] += 1
+
+                if rows[rowIndex][digit]! > 1 || columns[columnIndex][digit]! > 1  || boxes[boxIndex][digit]! > 1 {
+                    return false
+                }
             }
         }
         return true
